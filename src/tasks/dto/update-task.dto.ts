3cf104c3796964e5ec31cf0c957a3696a8d69ceb
Netsita7 <orthodoxawit7@gmail.com>
2025-09-06 13:46:44 +0300

    update task

--- src/tasks/dto/update-task.dto.ts
+++ src/tasks/dto/update-task.dto.ts
@@ -1,32 +1,3 @@
-// import { IsEnum, IsOptional, IsString, IsDateString } from 'class-validator';
-// import { TaskStatus, TaskPriority } from '../task.entity';
-
-// export class UpdateTaskDto {
-//   @IsEnum(TaskStatus)
-//   @IsOptional()
-//   status?: TaskStatus;
-
-//   @IsEnum(TaskPriority)
-//   @IsOptional()
-//   priority?: TaskPriority;
-
-//   @IsDateString()
-//   @IsOptional()
-//   dueDate?: Date;
-
-//   @IsString()
-//   @IsOptional()
-//   assigneeId?: string;
-
-//   @IsString()
-//   @IsOptional()
-//   title?: string;
-
-//   @IsString()
-//   @IsOptional()
-//   description?: string;
-// }
-// update-task.dto.ts
 import { IsEnum, IsOptional, IsString, IsDateString } from 'class-validator';
 import { TaskStatus, TaskPriority } from '../task.entity';
 import { ApiPropertyOptional } from '@nestjs/swagger';
